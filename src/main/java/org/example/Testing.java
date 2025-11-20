5e644dfc3296c54a471b3ac9636aeaa68163e14f
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-20 14:15:44 +0530

    Basic automation testing for dummy website

--- src/main/java/org/example/Testing.java
+++ src/main/java/org/example/Testing.java
@@ -1,11 +1,31 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Testing {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Browser\\chromedriver.exe");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\unknown user\\Documents\\Browser\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demo.nopcommerce.com/");
+        System.out.println("Page Title: " + driver.getTitle());
+
+        WebElement searchbar = driver.findElement(By.id("small-searchterms"));
+        WebElement searchbtn = driver.findElement(By.cssSelector(".button-1.search-box-button"));
+
+        searchbar.sendKeys("computer");
+        Thread.sleep(2000);
+
+        searchbtn.click();
+        Thread.sleep(2000);
+
+        WebElement loginnav = driver.findElement(By.className("ico-login"));
+        loginnav.click();
+
+        Thread.sleep(2000);
+        driver.quit();
     }
 }
