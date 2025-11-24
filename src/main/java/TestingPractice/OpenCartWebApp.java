6e50974705547e233b177dff68208ccbcb0b507b
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-24 22:43:47 +0530

    Selenium testing

--- src/main/java/TestingPractice/OpenCartWebApp.java
+++ src/main/java/TestingPractice/OpenCartWebApp.java
@@ -1,5 +1,6 @@
 package TestingPractice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,6 +8,8 @@ public class OpenCartWebApp {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.opencart.com/index.php?route=cms/demo");
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("Download")).click();
 
     }
 }
