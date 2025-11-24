91417c2b1c072d98ccab78f37361d88bc05a3a09
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-24 12:29:39 +0530

    Basic java code

---
+++ src/main/java/JavaPractice/Bus.java
@@ -0,0 +1,28 @@
+package JavaPractice;
+
+public class Bus implements Vehicle {
+    @Override
+    public void speed() {
+        System.out.println("Bus speed");
+    }
+
+    @Override
+    public void Start() {
+        System.out.println("Bus start");
+    }
+
+    @Override
+    public void mileage() {
+        System.out.println("Bus mileage");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Bus stop");
+    }
+
+    @Override
+    public void details() {
+        System.out.println("Details of Bus");
+    }
+}
---
+++ src/main/java/JavaPractice/Truck.java
@@ -0,0 +1,28 @@
+package JavaPractice;
+
+public class Truck implements Vehicle {
+    @Override
+    public void speed() {
+        System.out.println("Truck speed");
+    }
+
+    @Override
+    public void Start() {
+        System.out.println("Truck start");
+    }
+
+    @Override
+    public void mileage() {
+        System.out.println("Truck mileage");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Truck stop");
+    }
+
+    @Override
+    public void details() {
+        System.out.println("details of truck");
+    }
+}
