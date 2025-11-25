4ea60c9498a7fea3ea2e4058519288e6cd6755c5
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-25 10:42:42 +0530

    Selenium locators

--- src/main/java/TestingPractice/OpenCartWebApp.java
+++ src/main/java/TestingPractice/OpenCartWebApp.java
@@ -1,15 +1,19 @@
 package TestingPractice;
+import java.util.*;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenCartWebApp {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.opencart.com/index.php?route=cms/demo");
+        driver.get("https://www.opencart.com/");
         driver.manage().window().maximize();
         driver.findElement(By.linkText("Download")).click();
-
+        List <WebElement> image = driver.findElements(By.tagName("img"));
+        System.out.println("Count of image in the page :" +image.size());
+        driver.quit();
     }
 }
