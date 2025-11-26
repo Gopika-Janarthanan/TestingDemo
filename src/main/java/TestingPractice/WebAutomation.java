6ee4f64e0266a6af89fa542f721d3a5523c654ea
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-26 11:16:56 +0530

    Selenium css Selectors

--- src/main/java/TestingPractice/WebAutomation.java
+++ src/main/java/TestingPractice/WebAutomation.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class OpenCartWebApp {
+public class WebAutomation {
     public static void main(String[] args) {
-        //locators();
+        locators();
         cssSelectors();
 
     }
