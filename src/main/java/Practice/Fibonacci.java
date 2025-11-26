3c432a6e9af669aa68f90822377db373ff24f73a
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-26 22:40:36 +0530

    fibonacci series

---
+++ src/main/java/Practice/Fibonacci.java
@@ -0,0 +1,24 @@
+package Practice;
+
+public class Fibonacci {
+    //Find 1 - Nth Fibonacci number
+    static void fibo(int n){
+        int a=0;
+        int b=1;
+        int c=0;
+        for(int i=0;i<n;i++){
+            System.out.print(a +" ");
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+    }
+    //Find Nth Fibonacci number
+    static int fibonacci(int n){
+        if(n<2){
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
