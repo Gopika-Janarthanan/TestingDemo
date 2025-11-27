ff080c036a7a198bc908725c19da77440e96b693
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-27 17:16:48 +0530

    Armstrong number

--- src/main/java/Practice/Armstrong.java
+++ src/main/java/Practice/Armstrong.java
@@ -1,12 +1,21 @@
 package Practice;
 
 public class Armstrong {
-
+    //Check whether a number is Armstrong
     static boolean armstrong(int n){
         if(isam(n) == n){
             return true;
         }
         return false;
+    }
+
+    //Print all Armstrong numbers in a given range
+    static void arms(int n){
+        for(int i=0;i<=n;i++){
+            if(armstrong(i)){
+                System.out.print(i+" ");
+            }
+        }
     }
 
     static int isam(int n){
