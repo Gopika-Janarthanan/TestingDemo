824d06ea67c14bfce5e954892e0a044bbd25b365
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-27 22:06:38 +0530

    Perfect Number

---
+++ src/main/java/Practice/PerfectNumber.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        System.out.println(perfect(6)+" "+perfect(28)+" " +perfect(90));
+    }
+    static boolean perfect(int n){
+        int sum=0;
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0){
+                sum+=i;
+            }
+        }
+        return sum==n;
+    }
+}
