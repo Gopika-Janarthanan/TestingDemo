07d4f4fe6f4144ab26c0beab4ab46a11360f583b
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-27 17:39:42 +0530

    reverse a number

---
+++ src/main/java/Practice/Reverse.java
@@ -0,0 +1,21 @@
+package Practice;
+import java.lang.StringBuilder;
+public class Reverse {
+    //Reverse a number
+    static int reverse(int n){
+        int ans=0;
+        while(0<n) {
+            int d = n%10;
+            ans=ans*10+d;
+            n=n/10;
+        }
+        return ans;
+    }
+
+    static int rev(int n){
+        StringBuilder ans = new StringBuilder(String.valueOf(n));
+        return Integer.parseInt(ans.reverse().toString());
+    }
+
+
+}
