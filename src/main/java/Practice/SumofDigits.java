8a1c2844908fc15d71447fe4fc110d3d3ad7ae8d
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-11-28 08:40:08 +0530

    sum of digits

---
+++ src/main/java/Practice/SumofDigits.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class SumofDigits {
+
+    //Sum of digits of a number
+    static int sum(int n){
+        int ans=0;
+        while(0<n){
+            int d=n%10;
+            ans+=d;
+            n=n/10;
+        }
+        return ans;
+    }
+}
