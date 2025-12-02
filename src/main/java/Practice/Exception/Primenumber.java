b55a1d82bcc3012ac7f234925ffd49d3e84d6c4f
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-12-02 07:58:59 +0530

    Exception : prime number

--- src/main/java/Practice/Exception/Primenumber.java
+++ src/main/java/Practice/Exception/Primenumber.java
@@ -4,31 +4,28 @@ import java.util.Scanner;
 
 public class Primenumber {
     public static void main(String[] args) {
-        prime();
+        System.out.println(prime());
 
     }
     static boolean prime(){
-        try{
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter a num : ");
-            int num = input.nextInt();
-            for(int i=0;i<=num;i++){
-                if(num<=1){
-                    return false;
-                }
-                if(num%i==0){
-                    return false;
-                }
-                return true;
-            }
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a num : ");
+        int num;
+        try {
+            num = input.nextInt();
         }
         catch(Exception e){
-            System.out.println("Wrong input");
-
+            System.out.println("Input mismatch");
+            return false;
         }
-        finally {
-            System.out.println("Prime number");
+        if (num <= 1) {
+            return false;
         }
+        for(int i=2;i*i<=num;i++){
+            if(num%i==0){
+                return false;
+            }
+        }return true;
     }
 }
