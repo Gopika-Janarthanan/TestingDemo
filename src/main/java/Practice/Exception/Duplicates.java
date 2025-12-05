3108ffb5c2fa2a2c42b4b01c5c572e1c1d05d13f
Gopika-Janarthanan <gopikajanarthanan4@gmail>
2025-12-05 19:55:22 +0530

    Exception handling

---
+++ src/main/java/Practice/Exception/Duplicates.java
@@ -0,0 +1,24 @@
+package Practice.Exception;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Duplicates {
+    public static void main(String[] args) {
+        duplicates();
+    }
+    static void duplicates(){
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        arr.add(30);
+        arr.add(20);
+        HashSet<Integer> set = new HashSet<>();
+        set.addAll(arr);
+        for(int i : set){
+            System.out.println(i);
+        }
+
+    }
+}
