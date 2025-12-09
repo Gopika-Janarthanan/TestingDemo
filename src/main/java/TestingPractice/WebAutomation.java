78b1cacf5a8b8df19dd92967b0afa905281859e2
Gopika-Janarthanan <gopikajanarthanan4@gmail.com>
2025-12-09 09:41:36 +0530

    Orange HRM Webdriver practice

--- src/main/java/TestingPractice/WebAutomation.java
+++ src/main/java/TestingPractice/WebAutomation.java
@@ -9,7 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebAutomation {
     public static void main(String[] args) {
       //  locators();
-        cssSelectors();
+      // cssSelectors();
+        orageHrm();
 
     }
     static void locators(){
@@ -27,6 +28,24 @@ public class WebAutomation {
         d.manage().window().maximize();
         d.findElement(By.cssSelector("input#small-searchterms")).sendKeys("Computer");
         d.quit();
+    }
+    static void orageHrm( ){
+        WebDriver driver = new ChromeDriver();
+        //Website
+        driver.get("https://www.orangehrm.com/");
+        driver.manage().window().maximize();
+        driver.getTitle();
+        //declining
+        WebElement close = driver.findElement(By.xpath("//button[@id='CybotCookiebotDialogBodyButtonDecline' and  text() ='Deny']"));
+        if(close.isDisplayed()){
+            close.click();
+        }
+        //booking demo
+
+        WebElement booking =driver.findElement(By.xpath("//button[@class ='nav-link ' and  text() ='Book a Free Demo']"));
+        booking.click();
+
+
     }
 
 }
